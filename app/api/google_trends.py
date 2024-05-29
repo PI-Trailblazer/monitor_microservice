dde3d04f2a71a9dcf42da0488d494f8c26b2ae57
JakeRosa <joaquimvr15@gmail.com>
2024-05-29 03:34:32 +0100

    added predictor v1

--- app/api/google_trends.py
+++ app/api/google_trends.py
@@ -1,12 +1,17 @@
+import itertools
+from collections import defaultdict
+from datetime import datetime
+
 from serpapi import GoogleSearch
+
 from app.core.config import settings
 
 
-def get_trends(seacrh_term: str):
+def get_trends(seacrh_term: str, date: str):
     params = {
         "engine": "google_trends",
         "q": seacrh_term,
-        "cat": "208",
+        'date': date,
         "api_key": settings.GOOGLE_TRENDS_API_KEY,
     }
 
@@ -15,12 +20,73 @@ def get_trends(seacrh_term: str):
 
     return results
 
+# def get_slope_of_trend_last_3_hours(search_term: str):
+#     trends = get_trends(search_term, "now 4-H")
+#     # values = trends["interest_over_time"]["timeline_data"]
 
-def get_difference_between_recent_and_old_values(search_term: str):
-    trends = get_trends(search_term)
+#     return trends
+
+def get_slope_and_b_of_trend_last_3_days(search_term: str):
+    trends = get_trends(search_term, "today 1-m")
     values = trends["interest_over_time"]["timeline_data"]
 
-    recent_value = values[-1]["values"][0]["extracted_value"]
-    old_value = values[0]["values"][0]["extracted_value"]
+    # Calculate the slopes and b values
+    slope1 = values[-2]['values'][0]['extracted_value'] - values[-3]['values'][0]['extracted_value']
 
-    return recent_value - old_value
+    b1 = values[-3]['values'][0]['extracted_value']
+
+    slope2 = values[-1]['values'][0]['extracted_value'] - values[-2]['values'][0]['extracted_value']
+
+    b2 = - slope2 + values[-2]['values'][0]['extracted_value']
+
+    average_slope = (slope1 + slope2) / 2
+
+    average_b = (b1 + b2) / 2
+
+    return average_slope, average_b
+
+def get_slope_and_b_of_trend_last_3_months(search_term: str):
+    trends = get_trends(search_term, "today 3-m")
+    values = trends["interest_over_time"]["timeline_data"]
+
+    monthly_totals = defaultdict(int)
+    monthly_counts = defaultdict(int)
+
+    even_division = len(values) // 3
+
+    first_month = values[:even_division]
+    second_month = values[even_division:even_division * 2]
+    third_month = values[even_division * 2:]
+
+    for day in first_month:
+        value = day['values'][0]['extracted_value']
+
+        monthly_totals[0] += value
+        monthly_counts[0] += 1
+
+    for day in second_month:
+        value = day['values'][0]['extracted_value']
+
+        monthly_totals[1] += value
+        monthly_counts[1] += 1
+
+    for day in third_month:
+        value = day['values'][0]['extracted_value']
+
+        monthly_totals[2] += value    
+        monthly_counts[2] += 1
+    
+    
+    slopes = []
+    for i in range(2):
+        slope = (monthly_totals[i + 1]/monthly_counts[i + 1] - monthly_totals[i]/monthly_counts[i])
+        slopes.append(slope)
+    
+    average_slope = sum(slopes) / 2
+
+    b1 = monthly_totals[0]/monthly_counts[0]
+    b2 = monthly_totals[1]/monthly_counts[1] - slopes[0]
+
+    average_b = (b1 + b2) / 2
+
+    return average_slope, average_b
